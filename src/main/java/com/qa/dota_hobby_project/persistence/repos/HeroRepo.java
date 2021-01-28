fceea1b59420c54b62153fa1f9ef8234ba13a35c
QA-SamWilliams <swilliams@academytrainee.com>
2021-01-28 15:06:14 +0000

    Created the Hero repo

--- src/main/java/com/qa/dota_hobby_project/persistence/repos/HeroRepo.java
+++ src/main/java/com/qa/dota_hobby_project/persistence/repos/HeroRepo.java
@@ -1,5 +1,18 @@
 package com.qa.dota_hobby_project.persistence.repos;
 
-public class HeroRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.qa.dota_hobby_project.persistence.domain.Hero;
+
+@Repository
+public interface HeroRepo extends JpaRepository<Hero, Long> {
+	
+	// CRUD
+
+	// CREATE HERO'S
+	// READ HEROS'S
+	// UPDATE HEROS'S
+	// DELETE HEROS'S
+	
 }
