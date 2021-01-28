1ecaaa09ce14166a91f2a40ca438602e79ebcb45
QA-SamWilliams <swilliams@academytrainee.com>
2021-01-28 17:10:46 +0000

    Updated HeroServices.java

--- src/main/java/com/qa/dota_hobby_project/services/HeroServices.java
+++ src/main/java/com/qa/dota_hobby_project/services/HeroServices.java
@@ -1,5 +1,16 @@
 package com.qa.dota_hobby_project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.qa.dota_hobby_project.persistence.domain.Hero;
+import com.qa.dota_hobby_project.persistence.dto.HeroDTO;
+import com.qa.dota_hobby_project.persistence.repos.HeroRepo;
+
+@Service
 public class HeroServices {
 
 }
