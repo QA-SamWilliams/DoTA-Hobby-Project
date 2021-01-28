ecf89e1d6843f896924823994aa8a84b09a12b5d
QA-SamWilliams <swilliams@academytrainee.com>
2021-01-28 17:04:43 +0000

    Added content to the HeroServices.java and added modelmapper to the pom.xml

--- src/main/java/com/qa/dota_hobby_project/services/HeroServices.java
+++ src/main/java/com/qa/dota_hobby_project/services/HeroServices.java
@@ -1,5 +1,62 @@
 package com.qa.dota_hobby_project.services;
 
-public class HeroServices {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
+import com.qa.dota_hobby_project.persistence.domain.Hero;
+import com.qa.dota_hobby_project.persistence.dto.HeroDTO;
+import com.qa.dota_hobby_project.persistence.repos.HeroRepo;
+
+@Service
+public class HeroServices {
+	
+	private HeroRepo repo;
+	private ModelMapper mapper;
+	
+	@Autowired
+	public HeroServices(HeroRepo repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
+	private HeroDTO mapToDTO(Hero hero) {
+        return this.mapper.map(hero, HeroDTO.class);
+    }
+	
+	// POST - CREATE
+	public HeroDTO createHero(Hero hero) {
+		this.repo.save(hero);
+		return mapToDTO(hero);
+	}
+	
+	// GET - READALL
+	public List<HeroDTO> readAll() {
+		List<Hero> heroList = this.repo.findAll();
+		List<HeroDTO> heroListDTO = heroList.stream().map(this::mapToDTO).collect(Collectors.toList());
+		
+		return heroListDTO;
+	}
+	
+	// GET - READ
+	public HeroDTO readHero(Long id) {
+		return this.mapToDTO(this.repo.findById(id).orElseThrow());
+	}
+	
+	// PUT - UPDATE
+	public HeroDTO updateHero(Long id, Hero hero) {
+		Hero updatedHero = this.repo.findById(id).orElseThrow();
+		MyBeanUtils.mergeNotNull(hero, updatedHero);
+		return this.mapToDTO(this.repo.save(updatedHero));
+	}
+	
+	// DELETES
+	public boolean deleteHero(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
 }
