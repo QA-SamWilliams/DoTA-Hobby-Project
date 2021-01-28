bcf1b77558aa1aea8a9b58a01c4aaa2fcf77a08a
QA-SamWilliams <swilliams@academytrainee.com>
2021-01-28 16:10:34 +0000

    Added content to HeroDTO.java

--- src/main/java/com/qa/dota_hobby_project/persistence/dto/HeroDTO.java
+++ src/main/java/com/qa/dota_hobby_project/persistence/dto/HeroDTO.java
@@ -1,5 +1,27 @@
 package com.qa.dota_hobby_project.persistence.dto;
 
 public class HeroDTO {
+	
+	private Long ID;
 
+	private String name;
+	private String primaryAttribute;
+	private float STR;
+	private float STR_gain;
+	private float AGI;
+	private float AGI_gain;
+	private float INT;
+	private float INT_gain;
+	private int minDMG;
+	private int maxDMG;
+	private float healthRegen;
+	private float manaRegen;
+	private int moveSpeed;
+	
+	
+	public HeroDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
