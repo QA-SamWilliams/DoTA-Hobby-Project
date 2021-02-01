f57dbdbc8d6421e4aa2ab5ba59541a84a1dfc4f2
QA-SamWilliams <swilliams@academytrainee.com>
2021-02-01 11:10:52 +0000

    Final changes to the Hero feature branch

--- src/main/java/com/qa/dota_hobby_project/persistence/domain/Hero.java
+++ src/main/java/com/qa/dota_hobby_project/persistence/domain/Hero.java
@@ -4,59 +4,40 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 
 import com.sun.istack.NotNull;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Hero {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long ID;
-
+	@ManyToOne
+	private Item items;
 	@NotNull
 	private String name;
 	@NotNull
 	private String primaryAttribute;
 	@NotNull
 	private String localizedName;
-
-	public Long getID() {
-		return ID;
-	}
-
-	public void setID(Long iD) {
+	
+	public Hero(Long iD, Item items, String name, String primaryAttribute, String localizedName) {
+		super();
 		ID = iD;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+		this.items = items;
 		this.name = name;
-	}
-
-	public String getPrimaryAttribute() {
-		return primaryAttribute;
-	}
-
-	public void setPrimaryAttribute(String primaryAttribute) {
 		this.primaryAttribute = primaryAttribute;
-	}
-	
-	public String getLocalizedName() {
-		return localizedName;
-	}
-	
-	public void setLocalizedName(String localizedName) {
 		this.localizedName = localizedName;
 	}
+	
+	
+	
 }
--- src/main/java/com/qa/dota_hobby_project/persistence/dto/HeroDTO.java
+++ src/main/java/com/qa/dota_hobby_project/persistence/dto/HeroDTO.java
@@ -1,55 +1,22 @@
 package com.qa.dota_hobby_project.persistence.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HeroDTO {
 
 	private Long ID;
-
+	
 	private String name;
 	private String primaryAttribute;
 	private String localizedName;
 	
-
-	public HeroDTO() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Long getID() {
-		return ID;
-	}
-
-	public void setID(Long iD) {
-		ID = iD;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPrimaryAttribute() {
-		return primaryAttribute;
-	}
-
-	public void setPrimaryAttribute(String primaryAttribute) {
-		this.primaryAttribute = primaryAttribute;
-	}
-
-	public String getLocalizedName() {
-		return localizedName;
-	}
-
-	public void setLocalizedName(String localizedName) {
-		this.localizedName = localizedName;
-	}
-
-	@Override
-	public String toString() {
-		return "HeroDTO [ID=" + ID + ", name=" + name + ", primaryAttribute=" + primaryAttribute + ", localizedName="
-				+ localizedName + "]";
-	}
+	private List<ItemDTO> itemList;
 
 }
